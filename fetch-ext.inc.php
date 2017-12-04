235490ebf46a183e12297a390a8293f92fc6f75a
pete-harris-sf <peteharris@outlook.com>
2017-12-04 23:57:38 +0800

    Create fetch-ext.inc.php

---
+++ fetch-ext.inc.php
@@ -0,0 +1,12 @@
+function getter($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return $data;
+}
+
+echo getter('http://yourdomain.com/waktu-solat.php');
